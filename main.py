9a992856c19dc637770a539d58f8d213b374b64a
tusindfryd <20538005+tusindfryd@users.noreply.github.com>
2022-05-25 18:23:29 +0200

    add threads, yaw and distance control, change face model

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
-from dis import dis
-from math import dist
-import sys
 from djitellopy import Tello
 import cv2
 from time import sleep
+from datetime import datetime
+from threading import Thread
 
+global instructions
+global drone
 drone = Tello()
-drone.connect()
-drone.streamon()
+
+instructions = []
+
+
+def fly():
+    drone.connect()
+    print(f'Battery: {drone.get_battery()}')
+    drone.takeoff()
+    drone.move_up(80)
+    drone.streamon()
+    fly_instructions = []
+    done_instructions = []
+    while True:
+        sleep(0.1)
+        if instructions and instructions[-1] not in fly_instructions:
+            fly_instructions.append(instructions[-1])
+        for instruction in fly_instructions:
+            if instruction not in done_instructions:
+                print(instruction)
+                if instruction[0] == "right":
+                    print("moving right")
+                    drone.rotate_clockwise(15)
+                    sleep(5)
+                elif instruction[0] == "left":
+                    print("moving left")
+                    drone.rotate_counter_clockwise(15)
+                    sleep(5)
+                elif instruction[0] == "forward":
+                    print("moving forward")
+                    drone.move_forward(20)
+                elif instruction[0] == "backward":
+                    print("moving backward")
+                    drone.move_back(20)
+                done_instructions.append(instruction)
+            if len(done_instructions) == 3:
+                drone.land()
+
+
+fly_thread = Thread(target=fly, daemon=True)
+fly_thread.start()
+
 
 VIDEO_URL = drone.get_udp_video_address()
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface_improved.xml')
 
 counter = 1
-faces = []
-print(f'Battery: {drone.get_battery()}')
-drone.takeoff()
-drone.move_up(80)
-
-# while True:
-#     height = int(drone.send_read_command('height?')[:-2]) * 10
-#     if height <= 120:
-#         drone.move_up(20)
-#         sleep(1)
-#     else:
-#         break
-
-# drone.rotate_clockwise(360)
+face = [0, 0, 0, 0]
 
 capture = cv2.VideoCapture(VIDEO_URL)
 capture.open(VIDEO_URL)
@@ -36,36 +62,43 @@ frame_width, frame_height = capture.get(3), capture.get(4)
 while True:
     grabbed, frame = capture.read()
     if grabbed:
-        if counter % 10 == 0:
-            if len(faces) > 0:
-                face = sorted(faces, key=lambda face: face[2] * face[3])[-1]
-                for (x, y, w, h) in [face]:
-                    cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-
+        if counter % 10 == 0:  # every ten frames: detect faces
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-        if counter % 100 == 0:
             if len(faces) > 0:
-                distance_horizontal = (frame_width / 2) - ((x + w) / 2)
-                if abs(distance_horizontal) > 30:
-                    if distance_horizontal > 0:
-                        drone.move_right(20)
-                    elif distance_horizontal < 0:
-                        drone.move_left(20)
-                print(distance_horizontal)
-                counter = 1
+                # choose the biggest face
+                face = sorted(faces, key=lambda face: face[2] * face[3])[-1]
+            else:
+                face = [0, 0, 0, 0]
+
+        if counter % 100 == 0:  # every hundred frames: move
+            if all(face):
+                (x, y, w, h) = face
+                face_size = w * h
+                print(face_size)
+                horizontal_dist = (frame_width / 2) - (x + w / 2)
+                vertical_dist = (frame_height / 2) - (y + h / 2)
+                if face_size < 19000:
+                    instructions.append(["forward", datetime.now()])
+                elif face_size > 22000:
+                    instructions.append(["backward", datetime.now()])
+                if abs(horizontal_dist) > 30:
+                    if horizontal_dist < 0:
+                        instructions.append(["right", datetime.now()])
+                    elif horizontal_dist > 0:
+                        instructions.append(["left", datetime.now()])
+                    print(instructions)
+            counter = 1
+
         else:
             counter = counter + 1
 
-            # print(f'Frame: {frame_width}, {frame_height}, x: {x}, y: {y}')
-
-            # if distance_horizontal - frame_width / 2 > 20:
-            #     drone.move_right(20)
-            # elif distance_horizontal - frame_width / 2 < 20:
-            #     drone.move_left(20)
-            # drone.land()
-            # break
+        cv2.circle(frame, (int(frame_width / 2), int(frame_height / 2)),
+                   radius=2, color=(0, 255, 0), thickness=1)
+        for (x, y, w, h) in [face]:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.circle(frame, (int(x + w / 2), int(y + h / 2)),
+                       radius=2, color=(0, 0, 255), thickness=1)
 
         cv2.imshow('tello', frame)
     if cv2.waitKey(1) != -1:
